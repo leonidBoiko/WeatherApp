dbf7541df344096b00cc7773247b6ffe9fc039d0
Leonid.Boyko <leonid.boyko@computerstein.net>
2021-08-14 19:34:23 +0300

    new package, config splash screen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './src/router';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 import store from './src/store/index';
 
 const App = () => {
+  useEffect(() => SplashScreen.hide(), []);
+
   return (
     <Provider store={store}>
       <Router />
